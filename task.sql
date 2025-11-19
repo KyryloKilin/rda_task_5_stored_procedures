c43d7acd3d9f36f7690006f8cfe2437f04e7570f
root <root@vmi2772850.contaboserver.net>
2025-11-19 11:58:41 +0000

    Fix stored procedure without dropping ShopDB p.3

--- task.sql
+++ task.sql
@@ -1,13 +1,13 @@
-DROP DATABASE IF EXISTS ShopDB;
-CREATE DATABASE ShopDB;
 USE ShopDB;
 
 DELIMITER //
 
+DROP PROCEDURE IF EXISTS get_warehouse_product_inventory //
+
 CREATE PROCEDURE get_warehouse_product_inventory (IN p_warehouse_id INT)
 BEGIN
     SELECT
-        p.Name            AS ProductName,
+        p.Name             AS ProductName,
         pi.WarehouseAmount AS Count
     FROM ProductInventory AS pi
     JOIN Products        AS p ON pi.ProductID = p.ID
